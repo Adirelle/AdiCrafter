1458e481c45d9b8622a5e0b04f313aef1a9acb66
Adirelle <adirelle@gmail.com>
2021-12-30 00:18:33 +0100

    First version of the block and item.

--- src/main/kotlin/dev/adirelle/adicrafter/AdiCrafter.kt
+++ src/main/kotlin/dev/adirelle/adicrafter/AdiCrafter.kt
@@ -1,11 +1,31 @@
 package dev.adirelle.adicrafter
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
+import net.minecraft.block.Block
+import net.minecraft.block.Material
+import net.minecraft.item.BlockItem
+import net.minecraft.item.ItemGroup
+import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry
+import org.apache.logging.log4j.LogManager
+
+const val MOD_ID = "adicrafter"
+
+val LOGGER = LogManager.getLogger(MOD_ID)
+
+val CRAFTER_BLOCK = Block(FabricBlockSettings.of(Material.METAL).strength(4.0f))
+val CRAFTER_ITEM = BlockItem(CRAFTER_BLOCK, FabricItemSettings().group(ItemGroup.REDSTONE))
 
 @Suppress("UNUSED")
 object AdiCrafter : ModInitializer {
-    private const val MOD_ID = "adicrafter"
+
     override fun onInitialize() {
-        println("Example mod has been initialized.")
+        Registry.register(Registry.BLOCK, Identifier(MOD_ID, "crafter"), CRAFTER_BLOCK)
+        Registry.register(Registry.ITEM, Identifier(MOD_ID, "crafter"), CRAFTER_ITEM)
+        
+        LOGGER.info("${MOD_ID} initialized!")
     }
+
 }
