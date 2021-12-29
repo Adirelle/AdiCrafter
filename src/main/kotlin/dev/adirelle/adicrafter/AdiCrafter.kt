d2ae5cb7ea475a9e08bd3c2fba8ecb1a30372d63
Adirelle <adirelle@gmail.com>
2021-12-29 21:18:18 +0100

    First commit.

---
+++ src/main/kotlin/dev/adirelle/adicrafter/AdiCrafter.kt
@@ -0,0 +1,11 @@
+package dev.adirelle.adicrafter
+
+import net.fabricmc.api.ModInitializer
+
+@Suppress("UNUSED")
+object AdiCrafter : ModInitializer {
+    private const val MOD_ID = "adicrafter"
+    override fun onInitialize() {
+        println("Example mod has been initialized.")
+    }
+}
