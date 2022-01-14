8237fe181c934595733801ff821412aae89c8d49
Adirelle <adirelle@gmail.com>
2022-01-14 09:44:00 +0100

    (Try to) add a task to upload to Modrinth.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,14 @@
+import com.modrinth.minotaur.TaskModrinthUpload
+import com.modrinth.minotaur.request.VersionType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("fabric-loom")
 
     val kotlinVersion: String by System.getProperties()
     kotlin("jvm").version(kotlinVersion)
+
+    id("com.modrinth.minotaur").version("1.2.1")
 }
 
 base {
@@ -10,8 +16,17 @@ base {
     archivesName.set(archivesBaseName)
 }
 
-val minecraftVersion: String by project
+val env = System.getenv()
+
 val modVersion: String by project
+val releaseType = when {
+    "-alpha" in modVersion -> "ALPHA"
+    "-beta" in modVersion  -> "BETA"
+    else                   -> "RELEASE"
+}
+
+val minecraftVersion: String by project
+
 version = "${modVersion}+mc${minecraftVersion}"
 
 val mavenGroup: String by project
@@ -55,6 +70,25 @@ loom {
     }
 }
 
+task<TaskModrinthUpload>("modrinth") {
+    group = "upload"
+    onlyIf { env.contains("MODRINTH_TOKEN") }
+    dependsOn("build")
+
+    val modrinthProjectId: String by project
+
+    projectId = modrinthProjectId
+    token = env["MODRINTH_TOKEN"]
+    uploadFile = tasks["remapJar"]
+
+    versionNumber = version.toString()
+    versionName = modVersion
+    versionType = VersionType.valueOf(releaseType)
+    addGameVersion(minecraftVersion)
+
+    addLoader("fabric")
+}
+
 tasks {
     val javaVersion = JavaVersion.VERSION_17
 
@@ -65,7 +99,7 @@ tasks {
         options.release.set(javaVersion.toString().toInt())
     }
 
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    withType<KotlinCompile> {
         kotlinOptions { jvmTarget = javaVersion.toString() }
         sourceCompatibility = javaVersion.toString()
         targetCompatibility = javaVersion.toString()
@@ -85,3 +119,4 @@ tasks {
         withSourcesJar()
     }
 }
+
