96b132320124454015a0f44e74969f98a3ca7b92
Adirelle <adirelle@gmail.com>
2022-01-14 23:12:27 +0100

    Improved the crafter GUI.

--- src/main/kotlin/dev/adirelle/adicrafter/crafter/CrafterScreenHandler.kt
+++ src/main/kotlin/dev/adirelle/adicrafter/crafter/CrafterScreenHandler.kt
@@ -20,9 +20,8 @@ import io.github.cottonmc.cotton.gui.SyncedGuiDescription
 import io.github.cottonmc.cotton.gui.networking.NetworkSide.CLIENT
 import io.github.cottonmc.cotton.gui.networking.NetworkSide.SERVER
 import io.github.cottonmc.cotton.gui.networking.ScreenNetworking
-import io.github.cottonmc.cotton.gui.widget.WGridPanel
-import io.github.cottonmc.cotton.gui.widget.WItemSlot
-import io.github.cottonmc.cotton.gui.widget.WToggleButton
+import io.github.cottonmc.cotton.gui.widget.*
+import io.github.cottonmc.cotton.gui.widget.data.Texture
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant
@@ -32,13 +31,16 @@ import net.fabricmc.fabric.api.transfer.v1.storage.StorageUtil
 import net.fabricmc.fabric.api.transfer.v1.transaction.Transaction
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.entity.player.PlayerInventory
+import net.minecraft.inventory.Inventory
 import net.minecraft.item.ItemStack
 import net.minecraft.network.PacketByteBuf
 import net.minecraft.screen.ScreenHandlerContext
 import net.minecraft.screen.slot.Slot
 import net.minecraft.screen.slot.SlotActionType
 import net.minecraft.screen.slot.SlotActionType.*
-import net.minecraft.text.LiteralText
+import net.minecraft.text.TranslatableText
+import net.minecraft.util.Formatting.GRAY
+import net.minecraft.util.Formatting.WHITE
 import net.minecraft.util.Identifier
 import java.util.function.Consumer
 
@@ -61,10 +63,17 @@ class CrafterScreenHandler(
 
         private val FUZZY_MSG_ID = Identifier(AdiCrafter.MOD_ID, "screen-set-fuzzy")
         private val FLUID_MSG_ID = Identifier(AdiCrafter.MOD_ID, "screen-set-fluid")
+
+        private val TEXTURES = Texture(Identifier(AdiCrafter.MOD_ID, "textures/gui/crafter.png"))
+        private val ARROW_TEX = TEXTURES.withUv(0f, 0f, 1f, 17f / 53)
+        private val FLUID_OFF_TEX = TEXTURES.withUv(0f, 18f / 54, 17f / 35, 35f / 53)
+        private val FLUID_ON_TEX = TEXTURES.withUv(18f / 35, 18f / 54, 1f, 35f / 53)
+        private val FUZZY_OFF_TEX = TEXTURES.withUv(0f, 36f / 54, 17f / 35, 1f)
+        private val FUZZY_ON_TEX = TEXTURES.withUv(18f / 35, 36f / 54, 1f, 1f)
     }
 
-    private val fuzzyToggle = WToggleButton(LiteralText("Fuzzy"))
-    private val fluidToggle = WToggleButton(LiteralText("Fluids"))
+    private val fuzzyToggle = WFuzzyButton()
+    private val fluidToggle = WFluidButton()
 
     // Client-side constructor
     @Environment(EnvType.CLIENT)
@@ -94,16 +103,17 @@ class CrafterScreenHandler(
         val gridSlot = WItemSlot.of(blockInventory, GRID_FIRST_SLOT, GRID_WIDTH, GRID_HEIGHT)
         root.add(gridSlot, 0, 1)
 
-        val resultSlot = WItemSlot.of(blockInventory, RESULT_SLOT)
-        resultSlot.isModifiable = false
-        root.add(resultSlot, 4, 2)
+        val resultSlot = WResultSlot(blockInventory, RESULT_SLOT)
+        root.add(resultSlot, 5, 2)
+
+        root.add(WSprite(ARROW_TEX), 3, 2, 2, 1)
 
         val outputSlot = WItemSlot.outputOf(blockInventory, OUTPUT_SLOT)
         outputSlot.isInsertingAllowed = false
-        root.add(outputSlot, 6, 2)
+        root.add(outputSlot, 7, 2)
 
-        root.add(fuzzyToggle, 6, 0)
-        root.add(fluidToggle, 6, 1)
+        root.add(fuzzyToggle, 7, 0)
+        root.add(fluidToggle, 8, 0)
 
         root.add(createPlayerInventoryPanel(true), 0, 4)
 
@@ -192,4 +202,42 @@ class CrafterScreenHandler(
             StorageUtil.move(storage, PlayerInventoryStorage.of(inv), { true }, maxAmount, tx)
         } ?: 0L
 
+    private class WFuzzyButton : WToggleButton(FUZZY_ON_TEX, FUZZY_OFF_TEX) {
+
+        override fun addTooltip(tooltip: TooltipBuilder) {
+            tooltip.add(
+                TranslatableText("gui.adicrafter.fuzzy.tooltip.title")
+                    .formatted(WHITE),
+                TranslatableText(
+                    if (toggle) "gui.adicrafter.fuzzy.tooltip.enabled"
+                    else "gui.adicrafter.fuzzy.tooltip.disabled"
+                )
+                    .formatted(GRAY),
+            )
+        }
+    }
+
+    private class WFluidButton : WToggleButton(FLUID_ON_TEX, FLUID_OFF_TEX) {
+
+        override fun addTooltip(tooltip: TooltipBuilder) {
+            tooltip.add(
+                TranslatableText("gui.adicrafter.fluid.tooltip.title")
+                    .formatted(WHITE),
+                TranslatableText(
+                    if (toggle) "gui.adicrafter.fluid.tooltip.enabled"
+                    else "gui.adicrafter.fluid.tooltip.disabled"
+                )
+                    .formatted(GRAY),
+            )
+        }
+    }
+
+    private class WResultSlot(inv: Inventory, slot: Int) : WItemSlot(inv, slot, 1, 1, false) {
+
+        init {
+            isModifiable = false
+        }
+
+        override fun canFocus() = false
+    }
 }
